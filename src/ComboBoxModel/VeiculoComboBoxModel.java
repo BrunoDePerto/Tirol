817f8fbdd39ec53e38b2f347012dfd3d1ada30dc
ctb03 <ctb03@192.168.1.24>
2013-10-08 11:57:39 -0300

    Criando o ComboBoxModel

--- src/ComboBoxModel/VeiculoComboBoxModel.java
+++ src/ComboBoxModel/VeiculoComboBoxModel.java
@@ -1,7 +1,7 @@
 package ComboBoxModel;
 
-import Dao.Factory;
 import Model.Veiculo;
+import java.util.ArrayList;
 import java.util.List;
 import javax.swing.AbstractListModel;
 import javax.swing.MutableComboBoxModel;
@@ -10,55 +10,107 @@ import javax.swing.MutableComboBoxModel;
  *
  * @author Bruno
  */
+//http://www.guj.com.br/java/234433-problema-ao-selecionar-item-dentro-do-jcombobox
 public class VeiculoComboBoxModel extends AbstractListModel implements MutableComboBoxModel {
 
+    private Object selectedItem;
     private List<Veiculo> veiculos;
-    private int tamanho;
 
     public VeiculoComboBoxModel() {
+        //Criar um novo ArrayList vazio
+        veiculos = new ArrayList<>();
     }
 
     public VeiculoComboBoxModel(List<Veiculo> veiculos) {
+        //Cria o List veiculo com o valor da lista passada por parametro
+        this.veiculos = new ArrayList<>(veiculos);
+    }
+
+    public void setVeiculos(List<Veiculo> veiculos) {
+        //Seta uma lista de veiculos
         this.veiculos = veiculos;
     }
 
     @Override
     public int getSize() {
-        return this.veiculos.size();
+        //Retorna o tamanho da lista de veiculos
+        return veiculos.size();
     }
 
     @Override
     public Object getElementAt(int index) {
-        return this.veiculos.get(index);
-    }
-
-    @Override
-    public void addElement(Object item) {
-        
-    }
-
-    @Override
-    public void removeElement(Object obj) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public void insertElementAt(Object item, int index) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public void removeElementAt(int index) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public void setSelectedItem(Object anItem) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //Retorna o veiculo que esteja na posição indice
+        try {
+            return this.veiculos.get(index);
+        } catch (IndexOutOfBoundsException e) {
+            throw new IndexOutOfBoundsException("Valor fora da lista");
+        }
     }
 
     @Override
     public Object getSelectedItem() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //Retornar o elemento selecionado
+        return selectedItem;
+    }
+
+    public Integer getSelectedIndex() {
+        //Retorna o indice do elemento selecionado
+        int index = veiculos.indexOf(this.getSelectedItem());
+        return index;
+    }
+
+    @Override
+    public void setSelectedItem(Object anItem) {
+        //Seta o elemento selecionado
+        selectedItem = anItem;
+        fireContentsChanged(this, -1, veiculos.size());
+    }
+
+    public void setElementAt(Object veiculo, int index) {
+        //Seta o elemento selecionado na posição indice
+        veiculos.set(index, (Veiculo) veiculo);
+        int length = getSize();
+        fireIntervalAdded(this, length - 1, length - 1);
+    }
+
+    @Override
+    public void addElement(Object veiculo) {
+        this.veiculos.add((Veiculo) veiculo);
+        int length = getSize();
+        fireIntervalAdded(this, length - 1, length - 1);
+    }
+
+    @Override
+    public void insertElementAt(Object veiculo, int index) {
+        this.veiculos.add(index, (Veiculo) veiculo);
+        fireIntervalAdded(this, index, index);
+    }
+
+    @Override
+    public void removeElement(Object veiculo) {
+        int index = veiculos.indexOf(veiculo);
+        if (index != -1) {
+            // Remove an element  
+            veiculos.remove(veiculo);
+            // Removed from middle, notify ListDataListener objects  
+            fireIntervalRemoved(this, index, index);
+        }
+    }
+
+    public void removeAllItens() {
+        if (veiculos != null && veiculos.size() > 0) {
+            veiculos.clear();
+            fireContentsChanged(this, -1, veiculos.size());
+        }
+    }
+
+    @Override
+    public void removeElementAt(int index) {
+        if (getSize() >= index) {
+            // Remove an element at the specified position  
+            veiculos.remove(index);
+            // Removed from index, notify ListDataListener objects  
+            fireIntervalRemoved(this, index, index);
+        }
     }
 }
